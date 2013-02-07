da4594cc33bdef3db20de7a6a2c7a46eb7027052
Cory Bohon <cory@corybohon.com>
2013-02-06 20:09:39 -0500

    Made changes to the service to support errors, and started the process of adding system call functionality.

--- remote-liblouis/app.rb
+++ remote-liblouis/app.rb
@@ -4,18 +4,49 @@
 require 'sinatra'
 require 'json'
 
-get '/' do
+##User is trying to access an endpoint that isn't valid
+not_found do
 
-	"Only POST is allowed"
+	##If the user tries to access an invalid endpoint, give them an error and return documentation link
+	status 404
+	"Not found; only POST is allowed. See documentation here: --INSERT DOCUMENTATION LINK HERE FOR PRODUCTION--"
+
+end
+
+##User is posting to the /braille endpoint
+post '/braille' do
+
+	##Create the input and output files for the system call
+	input_file = Tempfile.new('braille')
+	output_file = Tempfile.new('braille')
 	
-end
+	"Input file created"
+	
+	##Write the message that the caller provided in the message param to the input_file var
+	input_file.write("#{params[:message]}")
+	
+	"Input file written"
+	
+	begin 
+		
+		##Make the system call 
+			system("xml2brl -p #{input_file.path} #{output_file.path}")
+		
+		"System call made"
+		
+		##Return the output file contents
+			"#{output_file.read}"
+	
+	ensure
+	
+		##Close the files
+		input_file.close
+		output_file.close
+	
+		##Unlink the files
+		input_file.unlink
+		output_file.unlink
+	
+	end
 
-### BrailleObject
-class BrailleObject
-  property :plaintext, String, :required => true
-  property :brailletext, String
-  property :status, String
 end
-
-## ThingResource application
-class RemoteLibLouis < Sinatra::Base

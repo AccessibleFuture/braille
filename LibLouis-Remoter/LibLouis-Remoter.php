af549165daca286b104521b34014cc6553168a7f
James Smith <jgsmith@gmail.com>
2013-05-14 09:51:17 -0400

    remove server/port assumption in remote php lib

--- LibLouis-Remoter/LibLouis-Remoter.php
+++ LibLouis-Remoter/LibLouis-Remoter.php
@@ -9,19 +9,11 @@
 */
 
 
-function returnBrailleForString($textToTranslate, $server, $port)
+function returnBrailleForString($textToTranslate, $url)
 {
 	//convert the passed content into an array, and then JSON encode it 
 	$content = json_encode(array("content" => $textToTranslate));
 	
-	//check to see if there's a port number, if so, add it to the server
-	if($port != ""){
-		$server = $server . ":" . $port;
-	}
-	
-	//add the braille.json endpoint to the 
-	$url = $server . "/braille.json";
-
 	//cURL implementation to handle posting the content and getting a response back
  	$curl = curl_init($url);
  	curl_setopt($curl, CURLOPT_HEADER, false);
